74eca5276d2a52284ec2bd24301262649e0c875d
Brandon Adonis <ac104133@avcol.school.nz>
2021-09-07 14:46:09 +1200

    I have created the Login and Register page with the scaffolding using MVC

---
+++ GamingWebApp/Areas/Identity/Data/GamingWebAppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace GamingWebApp.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the GamingWebAppUser class
+    public class GamingWebAppUser : IdentityUser
+    {
+    }
+}
---
+++ GamingWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GamingWebApp.Areas.Identity.Data;
+using GamingWebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GamingWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace GamingWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<GamingWebDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("GamingWebDbContextConnection")));
+
+                services.AddDefaultIdentity<GamingWebAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<GamingWebDbContext>();
+            });
+        }
+    }
+}
